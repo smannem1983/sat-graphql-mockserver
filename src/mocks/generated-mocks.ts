d5bbf1f9a0bae2aefd9d79b09d3676d333d839b0
Sandeep Mannem <sandeep.mannem@move.com>
2023-05-19 15:57:31 -0500

    Generate dates closer to current day

--- src/mocks/generated-mocks.ts
+++ src/mocks/generated-mocks.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-use-before-define,@typescript-eslint/no-unused-vars,no-prototype-builtins */
-import casual from 'casual';
+import { faker } from '@faker-js/faker';
 import { Query, TourAvailabilityDate, TourAvailabilityTime, TourData } from '../generated-types';
 
-casual.seed(0);
+faker.seed(0);
 
 export const aQuery = (overrides?: Partial<Query>): Query => {
     return {
@@ -12,25 +12,25 @@ export const aQuery = (overrides?: Partial<Query>): Query => {
 
 export const aTourAvailabilityDate = (overrides?: Partial<TourAvailabilityDate>): TourAvailabilityDate => {
     return {
-        id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : casual.uuid,
+        id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : faker.datatype.uuid(),
         tourAvailabilityTimes: overrides && overrides.hasOwnProperty('tourAvailabilityTimes') ? overrides.tourAvailabilityTimes! : [aTourAvailabilityTime(), aTourAvailabilityTime(), aTourAvailabilityTime()],
-        tourDate: overrides && overrides.hasOwnProperty('tourDate') ? overrides.tourDate! : casual['date'](),
+        tourDate: overrides && overrides.hasOwnProperty('tourDate') ? overrides.tourDate! : faker['date']['soon'](...[{"days":3}]),
     };
 };
 
 export const aTourAvailabilityTime = (overrides?: Partial<TourAvailabilityTime>): TourAvailabilityTime => {
     return {
-        endTime: overrides && overrides.hasOwnProperty('endTime') ? overrides.endTime! : casual['date'](),
-        id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : casual.uuid,
-        startTime: overrides && overrides.hasOwnProperty('startTime') ? overrides.startTime! : casual['date'](),
+        endTime: overrides && overrides.hasOwnProperty('endTime') ? overrides.endTime! : faker['date']['soon'](...[{"days":1}]).toISOString(),
+        id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : faker.datatype.uuid(),
+        startTime: overrides && overrides.hasOwnProperty('startTime') ? overrides.startTime! : faker['date']['soon'](...[{"days":1}]).toISOString(),
     };
 };
 
 export const aTourData = (overrides?: Partial<TourData>): TourData => {
     return {
         availableDates: overrides && overrides.hasOwnProperty('availableDates') ? overrides.availableDates! : [aTourAvailabilityDate(), aTourAvailabilityDate(), aTourAvailabilityDate()],
-        listingID: overrides && overrides.hasOwnProperty('listingID') ? overrides.listingID! : casual.uuid,
+        listingID: overrides && overrides.hasOwnProperty('listingID') ? overrides.listingID! : faker.datatype.uuid(),
     };
 };
 
-export const seedMocks = (seed: number) => casual.seed(seed);
+export const seedMocks = (seed: number) => faker.seed(seed);
